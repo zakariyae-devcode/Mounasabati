7bc4523001441fb6c065e56d64a2ed5251a0bc5b
zakariyaeAbbih <abbih1889@gmail.com>
2026-05-31 20:00:21 +0100

    commit-change-14

---
+++ backend/app/app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def hello_word():
+    return {"message":"Hello World"}
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-app=FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__=="__main__":
+   uvicorn.run("app.app:app",host="127.0.0.1",port=8000,reload=True)
