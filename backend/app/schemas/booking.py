5e8a85be9093c59a39515f002097d1f0cf5a0313
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-02 22:12:42 +0100

    commit-change-23

--- backend/app/schemas/booking.py
+++ backend/app/schemas/booking.py
@@ -5,18 +5,20 @@ from typing import Optional
 
 
 class BookingCreate(BaseModel):
-        event_date: date
-        event_time: time
-        total_price: float = Field(..., gt=0)
-        status: Optional[str] = "pending"
+    service_id: int
+    event_date: date
+    event_time: time
+    total_price: float = Field(..., gt=0)
+    status: Optional[str] = "pending"
 
 
 
 class BookingReceipt(BaseModel):
     booking_id: int
     service_name: str
+    vendor_name: str            # تمت الإضافة: ليكون الوصل أكثر دقة
     event_date: date
     event_time: time
     total_price: float
-    Status: str
+    status: str
     message: str = "تم تأكيد حجزك بنجاح! شكراً لاختيارك مناسبتي."
--- backend/app/schemas/service.py
+++ backend/app/schemas/service.py
@@ -1,25 +1,23 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field
 from typing import Optional
 
+class ServiceCreate(BaseModel):
+    service_name: str = Field(..., min_length=4, max_length=16)
+    price_base: float = Field(..., gt=0)
+    image: Optional[str] = None
+    status: Optional[str] = "active" 
 
+class ServiceUpdate(BaseModel):
+    service_name: Optional[str] = Field(None, min_length=4, max_length=16)
+    price_base: Optional[float] = Field(None, gt=0)
+    image: Optional[str] = None
 
-class VendorCreate(BaseModel):
-    ServiceName:str=Field(...,min_length=4,max_length=16)
-    Price_base:float = Field(..., gt=0)
-    Image:Optional[str]=None
-    status:Optional[str]=""
-    
+class ServiceOut(BaseModel):
+    id: int 
+    service_name: str
+    price_base: float
+    image: Optional[str] 
+    status: str
 
-class VendorUpdate(BaseModel):
-    ServiceName:Optional[str]=None
-    Price_base:Optional[float]=None
-    Image:Optional[str]=None
-
-class VendorOut(BaseModel):
-    ServiceName:str
-    Price_base:float
-    Image:str
-    status:str
-    
-    class config:
-        from_attributes=True
+    class Config: 
+        from_attributes = True
