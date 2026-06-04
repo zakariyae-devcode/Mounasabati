c10cf9d0f438cd4ea54554a3c9b17d8d678ec1f6
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-04 19:27:05 +0100

    commit-change-30

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,1 @@
-from app.core.database import Base
-from .service import Service
-from .client import Client
-from .reviews import Review
-from .booking import Booking
-from .user import User
+
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 import uvicorn
-from app.core.database import engine
-from app import models
-import os
-import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
 if __name__=="__main__":
-   models.Base.metadata.create_all(bind=engine)
+   
    uvicorn.run("app.app:app",host="127.0.0.1",port=8000,reload=True)
