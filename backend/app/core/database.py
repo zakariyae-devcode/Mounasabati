2b0ab876faa48f9f5129fbc807dacb3a2f53e967
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-04 19:42:48 +0100

    commit-change-32

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,19 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine,Session,SQLModel
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mounasabati.db"
 
-# قمت بتصحيح السطر أدناه:
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "sqlite:///./mounasabati.db"
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+engine=create_engine(DATABASE_URL, echo=True)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
