04c25f089532073c4a44a7c09fe14c3a84cdc9f3
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-13 12:28:48 +0100

    commit-change-62

--- backend/Accounts/models.py
+++ backend/Accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
+
 # Create your models here.
+
+class User(AbstractUser):
+    cin_validators=RegexValidator(
+        regex=r'^[A-z]{1,2}[0-9]{6,7}$'
+    )
+    class UserRole(models.TextChoices):
+        CLIENT="client","Client"
+        VENDOR="vendor","Vendor"
+        ADMIN="admin","Admin"
+    role=models.CharField(max_length=50,choices=UserRole.choices,default=UserRole.CLIENT)
+    cin=models.CharField(max_length=50,unique=True,validators=[cin_validators],verbose_name="رقم البطاقة الوطنية")
+    image = models.ImageField(upload_config='profile_pics/',null=True,blank=True)
+
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.username
+
+
