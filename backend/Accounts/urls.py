85d725240c707066f5e67b3f9a6c557dcee73e36
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-15 22:47:34 +0100

    commit-change-72

--- backend/Accounts/urls.py
+++ backend/Accounts/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from .views import hello
-from  .views import RegisterView,UserDetailView,UserUpdateView,ChangePasswordView,EmailResetView,ForgotPasswordView,LogoutView,AdminDeleteUserView
+from  .views import RegisterView,UserDetailView,UserUpdateView,ChangePasswordView,EmailResetView,ForgotPasswordView,LogoutView,AdminDeleteUserView,AdminUpdateUserRoleView,UserView
 
 urlpatterns = [
     path("", hello, name="hello"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("user/", UserDetailView.as_view(), name="user"),
-    path("user/update/", UserUpdateView.as_view(), name="user_update"),
-    path("user/change-password/", ChangePasswordView.as_view(), name="change_password"),
+    path("register/", RegisterView.as_view(), name="register-view"),
+    path("user/", UserDetailView.as_view(), name="user-details-view"),
+    path("user/update/", UserUpdateView.as_view(), name="user-update-view"),
+    path("user/change-password/", ChangePasswordView.as_view(), name="change-password-view"),
     path("user/email-reset/", EmailResetView.as_view(), name="email_reset"),
-    path("user/forgot-password/", ForgotPasswordView.as_view(), name="forgot_password"),
-    path("user/logout/", LogoutView.as_view(), name="logout"),
-    path("admin/delete-user/<str:user_cin>/", AdminDeleteUserView.as_view(), name="delete_user")
+    path("user/forgot-password/", ForgotPasswordView.as_view(), name="forgot-password-view"),
+    path("user/logout/", LogoutView.as_view(), name="logout-view"),
+    path("admin/delete-user/<str:user_cin>/", AdminDeleteUserView.as_view(), name="delete-user-view"),
+    path("admin/update-role/<str:user_cin>/",AdminUpdateUserRoleView.as_view(),name="update-role-view"),
+    path("admin/user/",UserView.as_view(),name="users-view")
 ]
