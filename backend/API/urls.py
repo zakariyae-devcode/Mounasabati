afd9ffc717313a40046f5a74f13a63785c22dd55
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-17 17:27:58 +0100

    commit-change-81

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/v1/admin/', admin.site.urls),
     path("api/v1/accounts",include("Accounts.urls")),
     path("api/v1/profiles",include("Profiles.urls")),
          
--- backend/Profiles/urls.py
+++ backend/Profiles/urls.py
@@ -1,8 +1,8 @@
+# Profiles/urls.py
 from django.urls import path
+from .views import CreateProfileView, UpdateProfileView
 
-from .views import CreateProfileView , UpdateProfileView
-
-urlpattrens=[
-    path("/create-profile",CreateProfileView.as_view(),name="create-profile"),
-    path("/update-profile",UpdateProfileView.as_view(),name="update-profile")
+urlpatterns = [
+    path('create/', CreateProfileView.as_view(), name='create-profile'),
+    path('update/', UpdateProfileView.as_view(), name='update-profile'),
 ]
