f04f7a8fa4521530e5c4855e20eaeacac1f6d855
zakariyaeAbbih <abbih1889@gmail.com>
2026-06-19 21:33:15 +0100

    commit-change-82

--- backend/Accounts/models.py
+++ backend/Accounts/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
 
 # Create your models here.
 
+
+class UsersManager(BaseUserManager):
+    def create_user(self, username,email,password=None, **extra_fields):
+        if not email:
+            raise ValueError("يجب ادخال البريد الاكتروني")
+        email=self.normalize_email(email)
+        extra_fields.setdefault("role","client")
+        user=self.model(username=username,email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        return self.create_user(username, email, password, **extra_fields)
 class Users(AbstractUser):
     cin_validators=RegexValidator(
         regex=r'^[A-Z]{1,2}[0-9]{6,7}$'
@@ -20,9 +37,24 @@ class Users(AbstractUser):
 
     deletion_requested_at = models.DateTimeField(null=True, blank=True)
 
+
+
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
 
+
+    objects = UsersManager()
+
+   
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'cin']
+
+    class Meta:
+        verbose_name = "مستخدم"
+        verbose_name_plural = "المستخدمين"
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
 
 
     def __str__(self):
