826df46bbbb780b7920de20bca5a5a4dc501c1f5
zakariyaeAbbih <abbih1889@gmail.com>
2026-07-11 18:25:24 +0100

    commit-change-102

--- backend/Notifications/apps.py
+++ backend/Notifications/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class NotificationsConfig(AppConfig):
-    name = 'Notifications'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Notifications' # أو المسار الكامل للتطبيق حسب مشروعك
+
+    def ready(self):
+        # استدعاء ملف الـ signals عند جاهزية التطبيق ليتم تفعيل المراقبة
+        import Notifications.signals
