d3078c6ef809f8fe00148212b31ec050a0b7e5a1
zakariyaeAbbih <abbih1889@gmail.com>
2026-07-11 18:48:48 +0100

    commit-change-104

--- backend/API/settings.py
+++ backend/API/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'django_filters',
+    'drf_yasg',
 
     #------------------APP-------------#
     'Accounts',
--- backend/API/urls.py
+++ backend/API/urls.py
@@ -1,13 +1,25 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="منصة الحجوزات والخدمات API",
+      default_version='v1',
+      description="توثيق كامل وشامل لجميع روابط النظام (الحجوزات، المدفوعات، التقييمات، المحادثات)",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="support@myproject.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True, # جعلها عامة ليراها مطورو الفرونت إند
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     
@@ -21,6 +33,10 @@ urlpatterns = [
     path("api/v1/notifications",include("Notifications.urls")),
     path("api/v1/conversations",include("Conversations.urls")),
     path("api/v1/payments",include("Payments.urls")),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
          
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
